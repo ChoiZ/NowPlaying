e47baa83590a9d12579e5c20687e0eeaa9096646
Michael Gauthier <mike@silverorange.com>
2013-11-18 12:07:55 -0400

    Whitespace.

--- www/NowPlaying.js
+++ www/NowPlaying.js
@@ -9,21 +9,21 @@
 (function(){
 
 	var cordovaRef = window.PhoneGap || window.Cordova || window.cordova; // old to new fallbacks
-	
+
 	var NowPlaying = function() {};
-	
+
 	/**
 	 * Update metadatas.
 	 */
 	NowPlaying.prototype.updateMetas = function(artist,title,album,cover) {
 		cordovaRef.exec(null, null, "NowPlaying", "updateMetas", [artist,title,album,cover]);
 	};
-	
+
 	cordovaRef.addConstructor(function(){
 		if (!window.plugins) {
 			window.plugins = {};
 		}
 		window.plugins.nowPlaying = new NowPlaying();
 	});
-	
+
 })();
